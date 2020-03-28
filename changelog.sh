8c2a864c1ec5b06ef4e675526b76921287b55785
Chad Metcalf <metcalfc@gmail.com>
2020-03-28 15:26:23 -0700

    Fix minor spacing issue.

--- changelog.sh
+++ changelog.sh
@@ -5,4 +5,4 @@ head_ref=$1
 base_ref=$2
 repo_url=$3
 
-git log "${base_ref}...${head_ref}" --pretty=format:"  * [view commit](http://github.com/${repo_url}/commit/%H) -  %s" --reverse
+git log "${base_ref}...${head_ref}" --pretty=format:"* [view commit](http://github.com/${repo_url}/commit/%H) -  %s" --reverse
