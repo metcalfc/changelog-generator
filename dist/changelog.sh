ed5b53ead696b12daf223854b864f102d08f3985
Chad Metcalf <metcalfc@gmail.com>
2020-08-31 14:53:25 -0700

    Ensure there is a default base ref.
    This should fix #14 and #20.
    
    Signed-off-by: Chad Metcalf <metcalfc@gmail.com>

--- dist/changelog.sh
+++ dist/changelog.sh
@@ -12,6 +12,14 @@ git fetch --depth=1 origin +refs/tags/*:refs/tags/* 1>&2
 git fetch --no-tags --prune --depth=1 origin +refs/heads/*:refs/remotes/origin/* 1>&2
 git fetch --prune --unshallow 1>&2
 
+# if folks don't have a base ref to compare against just use the initial
+# commit. This will show all the changes since the beginning but I can't
+# think of a better default.
+if [ -z "$base_ref" ]
+then
+  base_ref=$(git rev-list --max-parents=0 HEAD)
+fi
+
 git log "${base_ref}...${head_ref}" \
   --pretty=format:"* [\`%h\`](http://github.com/${repo_url}/commit/%H) - %s" \
   --reverse
