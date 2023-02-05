11c453599c9735c0d0e3db400f8842f65d3b0178
Niels ten Boom <nielstenboom@gmail.com>
2023-02-05 11:35:59 +0100

    remove hardcoded test branch

--- dist/changelog.sh
+++ dist/changelog.sh
@@ -13,13 +13,13 @@ fi
 # By default a GitHub action checkout is shallow. Get all the tags, branches,
 # and history. Redirect output to standard error which we can collect in the
 # action.
-# git fetch --depth=2 origin +refs/tags/*:refs/tags/* 1>&2
-# git fetch --no-tags --prune --depth=1 origin +refs/heads/*:refs/remotes/origin/* 1>&2
-# for d in .git/refs/heads/*; do
-#   dir=$(basename "${d}")
-#   git fetch --no-tags --prune --depth=1 origin +refs/heads/"${dir}"/*:refs/remotes/origin/"${dir}"/* 1>&2
-# done
-git fetch origin 1>&2
+git fetch --depth=2 origin +refs/tags/*:refs/tags/* 1>&2
+git fetch --no-tags --prune --depth=1 origin +refs/heads/*:refs/remotes/origin/* 1>&2
+for d in .git/refs/heads/*; do
+  dir=$(basename "${d}")
+  git fetch --no-tags --prune --depth=1 origin +refs/heads/"${dir}"/*:refs/remotes/origin/"${dir}"/* 1>&2
+done
+git fetch --prune --unshallow 1>&2
 
 # if folks don't have a base ref to compare against just use the initial
 # commit. This will show all the changes since the beginning but I can't
@@ -30,8 +30,6 @@ then
 fi
 
 # git log test/branch
-
-# git checkout "${base_ref}"
 
 # Bash quoting will get you. Do not quote the extra_flags. If its null
 # we want it to disappear. If you quote it, it will go to git as an ""
