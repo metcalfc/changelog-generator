ab80bdcd3d2691fdcbaef5aaa7e375afecabf584
Niels ten Boom <nielstenboom@gmail.com>
2023-02-05 11:09:00 +0100

    comment the fetches

--- changelog.sh
+++ changelog.sh
@@ -13,9 +13,9 @@ fi
 # By default a GitHub action checkout is shallow. Get all the tags, branches,
 # and history. Redirect output to standard error which we can collect in the
 # action.
-git fetch --depth=1 origin +refs/tags/*:refs/tags/* 1>&2
-git fetch --no-tags --prune --depth=1 origin +refs/heads/*:refs/remotes/origin/* 1>&2
-git fetch --prune --unshallow 1>&2
+# git fetch --depth=1 origin +refs/tags/*:refs/tags/* 1>&2
+# git fetch --no-tags --prune --depth=1 origin +refs/heads/*:refs/remotes/origin/* 1>&2
+# git fetch --prune --unshallow 1>&2
 
 # if folks don't have a base ref to compare against just use the initial
 # commit. This will show all the changes since the beginning but I can't
